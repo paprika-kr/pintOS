91c3093c928ffeb485046819bb22d71ff780bf5b
paprika-kr <feira@naver.com>
2021-01-06 18:16:52 +0900

    Init Project

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include "list.h"
+#include "hash.h"
+#include "bitmap.h"
+#include "round.h"
+#include "debug.h"
+#include "limits.h"
+
+int main(){
+	char inst[50];
+
+	fgets(inst, sizeof(inst), stdin);
+	inst[strlen(inst)-1] = '\0';
+	str = strtok(inst, " ");
+
+	if(strcmp(str, "create") = 0;
+	{
+		str = strtok(NULL, " ");
+		if(strcmp(str, "list") == 0)
+		{
+		}
+	}
+}
